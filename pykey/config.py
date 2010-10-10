496e515b11b63b00c1efa5791dacf9edddfe0fc4
admire93 <admire9@gmail.com>
2010-10-10 21:20:19 +0900

    Implement Bendy to excute command

--- pykey/config.py
+++ pykey/config.py
@@ -1,6 +1,16 @@
+import pykey.func
+
+def get_init_env():
+    envs = Environment()
+    envs['set!'] = pykey.func.set_value 
+    envs['get'] = pykey.func.get_value 
+    envs['del'] = pykey.func.del_value 
+    return envs
+
 conf = {
   "host": "127.0.0.1",
   "port": 7777,
   "notice": "pykey :: type (help) more information\n",
   "cmdline": "@> ",
+  "intial_env": get_init_envs()
 }
