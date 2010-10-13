8f0081d83e2b86ad4740df72a79e0ddfd0506972
admire93 <admire9@gmail.com>
2010-10-13 16:08:31 +0900

    Fix bug when occured query.st dosen't exists

--- pykey/run.py
+++ pykey/run.py
@@ -23,13 +23,14 @@ def main():
     sock = sv.get_sock()
     keys = pykey.memory.Key()
     fm = pykey.manage.FileManager()
-    query_manager = pykey.memory.Query()
-    query_manager.stack = fm.read_query()
-    query_manager.save()
-    try:
+    if os.path.isfile(config.conf["query_file"]):
+        query_manager = pykey.memory.Query()
+        query_manager.stack = fm.read_query()
+        query_manager.save()
+    if os.path.isfile(config.conf["file"]):
         file_size = os.path.getsize(config.conf["file"])
         keys.last = (file_size / config.conf["page"]) - 1
-    except OSError:
+    else:
         fm.initialize_file(config.conf["file"])
         keys.last = 0
     for k, i in fm.read_all_keys():
